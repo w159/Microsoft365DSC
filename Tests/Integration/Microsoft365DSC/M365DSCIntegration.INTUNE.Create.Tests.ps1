922406281aaabb67cd37b147a8f464781af8a868
NikCharlebois <nicharl@microsoft.com>
2024-10-26 14:41:46 +0000

    Updated {Create} Intune Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.INTUNE.Create.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.INTUNE.Create.Tests.ps1
@@ -2237,6 +2237,27 @@
                     TenantId              = $TenantId;
                     CertificateThumbprint = $CertificateThumbprint;
                 }
+                IntuneDeviceManagmentAndroidDeviceOwnerEnrollmentProfile 'IntuneDeviceManagmentAndroidDeviceOwnerEnrollmentProfile-MyTestEnrollmentProfile'
+                {
+                    AccountId                 = "8d2ac1fd-0ac9-4047-af2f-f1e6323c9a34e";
+                    ApplicationId             = $ApplicationId;
+                    CertificateThumbprint     = $CertificateThumbprint;
+                    ConfigureWifi             = $True;
+                    Description               = "This is my enrollment profile";
+                    DisplayName               = "MyTestEnrollmentProfile";
+                    EnrolledDeviceCount       = 0;
+                    EnrollmentMode            = "corporateOwnedDedicatedDevice";
+                    EnrollmentTokenType       = "default";
+                    EnrollmentTokenUsageCount = 0;
+                    Ensure                    = "Present";
+                    IsTeamsDeviceProfile      = $False;
+                    RoleScopeTagIds           = @("0");
+                    TenantId                  = $TenantId;
+                    TokenCreationDateTime     = "10/26/2024 1:02:29 AM";
+                    TokenExpirationDateTime   = "10/31/2024 3:59:59 AM";
+                    WifiHidden                = $False;
+                    WifiSecurityType          = "none";
+                }
                 IntuneDeviceRemediation 'ConfigureDeviceRemediation'
                 {
                     Assignments              = @(
