13499ebd66c05c69680e5d2df8ea198d8af68535
Kartikeya Saxena (from Dev Box) <kasaxena@microsoft.com>
2024-10-29 17:59:26 +0530

    Adding Examples

--- Modules/Microsoft365DSC/Examples/Resources/AADVerifiedIdAuthorityContract/3-Remove.ps1
+++ Modules/Microsoft365DSC/Examples/Resources/AADVerifiedIdAuthorityContract/3-Remove.ps1
@@ -22,18 +22,73 @@ Configuration Example
 
     node localhost
     {
-        AADVerifiedIdAuthority 'AADVerifiedIdAuthority-Contoso'
+        AADVerifiedIdAuthorityContract 'AADVerifiedIdAuthorityContract-Sample Custom Verified Credentials'
         {
-            DidMethod            = "web";
+            displays             = @(
+                MSFT_AADVerifiedIdAuthorityContractDisplayModel{
+                    consent = MSFT_AADVerifiedIdAuthorityContractDisplayConsent{
+                        instructions = 'Sign in with your account to get your card.'
+                        title = 'Do you want to get your Verified Credential?'
+                    }
+                    card = MSFT_AADVerifiedIdAuthorityContractDisplayCard{
+                        description = 'Use your verified credential to prove to anyone that you know all about verifiable credentials.'
+                        issuedBy = 'Microsoft'
+                        backgroundColor = '#000000'
+                        textColor = '#ffffff'
+                        logo = MSFT_AADVerifiedIdAuthorityContractDisplayCredentialLogo{
+                            uri = 'https://didcustomerplayground.z13.web.core.windows.net/VerifiedCredentialExpert_icon.png'
+                            description = 'Verified Credential Expert Logo'
+                        }
+                        title = 'Verified Credential Expert'
+                    }
+                    locale = 'en-US'
+                    claims = @(
+                        MSFT_AADVerifiedIdAuthorityContractDisplayClaims{
+                            label = 'First name'
+                            claim = 'vc.credentialSubject.firstName'
+                            type = 'String'
+                        }
+                        MSFT_AADVerifiedIdAuthorityContractDisplayClaims{
+                            label = 'Last name'
+                            claim = 'vc.credentialSubject.lastName'
+                            type = 'String'
+                        }
+                    )
+
+                }
+            );
             Ensure               = "Absent";
-            KeyVaultMetadata     = MSFT_AADVerifiedIdAuthorityKeyVaultMetadata{
-                SubscriptionId = '2ff65b89-ab22-4489-b84d-e60d1dc30a62'
-                ResourceName = 'xtakeyvault'
-                ResourceUrl = 'https://xtakeyvault.vault.azure.net/'
-                ResourceGroup = 'TBD'
+            linkedDomainUrl      = "https://$OrganizationName/";
+            name                 = "Sample Custom Verified Credentials";
+            rules                = MSFT_AADVerifiedIdAuthorityContractRulesModel{
+                validityInterval = 2592000
+                vc = MSFT_AADVerifiedIdAuthorityContractVcType{
+                    type = @('VerifiedCredentialExpert')
+                }
+                            attestations = MSFT_AADVerifiedIdAuthorityContractAttestations{
+                    idTokenHints = @(
+                        MSFT_AADVerifiedIdAuthorityContractAttestationValues{
+                            mapping = @(
+                                MSFT_AADVerifiedIdAuthorityContractClaimMapping{
+                                    inputClaim = '$.given_name'
+                                    indexed = $False
+                                    outputClaim = 'firstName'
+                                    required = $True
+                                }
+                                MSFT_AADVerifiedIdAuthorityContractClaimMapping{
+                                    inputClaim = '$.family_name'
+                                    indexed = $True
+                                    outputClaim = 'lastName'
+                                    required = $True
+                                }
+                            )
+                            required = $False
+                        }
+                    )
+
+                }
+            
             };
-            LinkedDomainUrl      = "https://nik-charlebois.com/";
-            Name                 = "Contoso";
             ApplicationId         = $ApplicationId
             TenantId              = $TenantId
             CertificateThumbprint = $CertificateThumbprint
