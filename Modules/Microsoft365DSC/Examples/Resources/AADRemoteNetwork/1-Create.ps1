8c24f957245ae8b83ac9188a53aaacbf9e8a60de
Piyush Dubey <pidubey@microsoft.com>
2024-10-30 18:46:53 +0530

    minor

--- Modules/Microsoft365DSC/Examples/Resources/AADRemoteNetwork/1-Create.ps1
+++ Modules/Microsoft365DSC/Examples/Resources/AADRemoteNetwork/1-Create.ps1
@@ -21,6 +21,45 @@ Configuration Example
     Import-DscResource -ModuleName Microsoft365DSC
     node localhost
     {
-        
+        AADRemoteNetwork "AADRemoteNetwork-Test Remote Network"
+        {
+            Ensure                = "Present";
+            ForwardingProfiles    = @("Microsoft 365 traffic forwarding profile");
+            Id                    = "c60c41bb-e512-48e3-8134-c312439a5343";
+            Name                  = "Test Remote Network";
+            Region                = "australiaSouthEast";
+            ApplicationId         = $ApplicationId
+            TenantId              = $TenantId
+            CertificateThumbprint = $CertificateThumbprint
+            DeviceLinks           = @(
+                MSFT_AADRemoteNetworkDeviceLink {
+                    Name                    = 'Test Link'
+                    IPAddress               = '1.1.1.1'
+                    BandwidthCapacityInMbps = 'mbps500'
+                    DeviceVendor            = 'ciscoCatalyst'
+                    BgpConfiguration        = MSFT_AADRemoteNetworkDeviceLinkbgpConfiguration {
+                        Asn                 = 82
+                        LocalIPAddress      = '1.1.1.87'
+                        PeerIPAddress       = '1.1.1.2'
+                    }
+                    RedundancyConfiguration = MSFT_AADRemoteNetworkDeviceLinkRedundancyConfiguration {
+                        RedundancyTier      = 'zoneRedundancy'
+                        ZoneLocalIPAddress  = '1.1.1.8'
+                    }
+                    TunnelConfiguration     = MSFT_AADRemoteNetworkDeviceLinkTunnelConfiguration {
+                        PreSharedKey               = 'blah'
+                        ZoneRedundancyPreSharedKey = 'blah'
+                        SaLifeTimeSeconds          = 300
+                        IPSecEncryption            = 'gcmAes192'
+                        IPSecIntegrity             = 'gcmAes192'
+                        IKEEncryption              = 'aes192'
+                        IKEIntegrity               = 'gcmAes128'
+                        DHGroup                    = 'ecp256'
+                        PFSGroup                   = 'pfsmm'
+                        ODataType                  = '#microsoft.graph.networkaccess.tunnelConfigurationIKEv2Custom'
+                    }
+                }
+            );
+        }
     }
 }
