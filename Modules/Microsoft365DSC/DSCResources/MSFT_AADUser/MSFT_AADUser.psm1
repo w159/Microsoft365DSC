a6666e9c203e454e1d8847a7fa000f5ce2cead58
Santiago Nuñez (Microsoft) <snunez@live.com>
2024-11-18 19:37:55 +0100

    fix BadRequest: Invalid filter clause, unterminated string literal when trying to remove AADUser group membership

--- Modules/Microsoft365DSC/DSCResources/MSFT_AADUser/MSFT_AADUser.psm1
+++ Modules/Microsoft365DSC/DSCResources/MSFT_AADUser/MSFT_AADUser.psm1
@@ -619,7 +619,6 @@ function Set-TargetResource
                 # user is a member of some groups, ensure that user is only a member of groups listed in MemberOf
                 Compare-Object -ReferenceObject $MemberOf -DifferenceObject $user.MemberOf | ForEach-Object {
                     $group = Get-MgGroup -Filter "DisplayName eq '$($_.InputObject)'" -Property Id, GroupTypes
-
                     if ($_.SideIndicator -eq '<=')
                     {
                         # Group in MemberOf not present in groups that user is a member of, add user to group
