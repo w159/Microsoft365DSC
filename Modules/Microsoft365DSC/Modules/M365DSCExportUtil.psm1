a94ac9ae14415d21f09388686fd884a34ca17294
Fabien Tschanz <fabien.tschanz@bluewin.ch>
2026-09-16 23:55:10 +0200

    Add missing export collection consumer map entry

--- Modules/Microsoft365DSC/Modules/M365DSCExportUtil.psm1
+++ Modules/Microsoft365DSC/Modules/M365DSCExportUtil.psm1
@@ -1919,6 +1919,7 @@ function Get-M365DSCExportCollectionConsumerMap
             'IntuneDeviceConfigurationTrustedCertificatePolicyWindows10',
             'IntuneDeviceConfigurationVpnPolicyWindows10',
             'IntuneDeviceConfigurationWindowsTeamPolicyWindows10',
+            'IntuneDeviceConfigurationWiredNetworkPolicyMacOS',
             'IntuneDeviceConfigurationWiredNetworkPolicyWindows10',
             'IntuneDeviceFeaturesConfigurationPolicyIOS',
             'IntuneTrustedRootCertificateAndroidDeviceOwner',
